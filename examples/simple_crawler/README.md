64860e38253a73bb01e174c1c3a13b0e84df3f74
Sylvain Zimmer <sylvain@sylvainzimmer.com>
2015-02-20 20:19:02 +0100

    Added simple crawler example for #63

---
+++ examples/simple_crawler/README.md
@@ -0,0 +1,63 @@
+Simple Web Crawler with MRQ
+===========================
+
+This is a simple demo app that crawls a website, to demo some MRQ features.
+
+
+How to use
+==========
+
+First, get into a Python virtualenv (`make virtualenv`) or into the docker image at the root of this directory (`make ssh`)
+
+Then install MRQ and the packages needed for this example:
+```
+$ cd examples/simple_crawler
+$ sudo apt-get install libxml2-dev libxslt1-dev libz-dev     # Needed for lxml
+$ pip install -r requirements.txt
+```
+
+Launch MongoDB & Redis if they are not already started:
+```
+$ mongod &
+$ redis-server &
+```
+
+Queue the first task via the command line:
+
+```
+$ mrq-run --queue crawl crawler.Fetch '{"url": "http://docs.python-requests.org/"}'
+```
+
+Then start a worker with 3 (or more!) greenlets:
+
+```
+$ mrq-worker crawl --greenlets 3
+```
+
+You should also launch a dashboard to monitor the progress:
+```
+$ mrq-dashboard
+```
+
+We also included 2 utility tasks:
+```
+$ mrq-run crawler.Report
+$ mrq-run crawler.Reset
+```
+
+This is obviously a very simple crawler, production systems will be much more complex but it gives you an overview of MRQ and a good starting point.
+
+
+Expected result for crawler.Report
+==================================
+
+As of 2015-02-20:
+
+```
+Crawl stats
+===========
+URLs queued: 26
+URLs successfully crawled: 23
+URLs redirected: 1
+Bytes fetched: 608131
+```
---
+++ examples/simple_crawler/crawler.py
@@ -0,0 +1,109 @@
+import requests
+import lxml.html
+import datetime
+import re
+import urlparse
+from mrq.context import connections, log
+from mrq.job import queue_job
+from mrq.task import Task
+from mrq.queue import Queue
+
+
+class Fetch(Task):
+
+    def run(self, params):
+
+        collection = connections.mongodb_jobs.simple_crawler_urls
+
+        response = requests.get(params["url"])
+
+        if response.status_code != 200:
+            log.warning("Got status %s on page %s (Queued from %s)" % (
+                response.status_code, response.url, params.get("from")
+            ))
+            return False
+
+        # Store redirects
+        if response.url != params["url"]:
+            collection.update({"_id": params["url"]}, {"$set": {
+                "redirected_to": response.url,
+                "fetched_date": datetime.datetime.now()
+            }})
+
+        document = lxml.html.fromstring(response.content)
+
+        document.make_links_absolute(response.url)
+
+        queued_count = 0
+
+        document_domain = urlparse.urlparse(response.url).netloc
+
+        for (element, attribute, link, pos) in document.iterlinks():
+
+            link = re.sub("#.*", "", link or "")
+
+            if not link:
+                continue
+
+            domain = urlparse.urlparse(link).netloc
+
+            # Don't follow external links for this example
+            if domain != document_domain:
+                continue
+
+            # We don't want to re-queue URLs twice. If we try to insert a duplicate,
+            # pymongo will throw an error
+            try:
+                collection.insert({"_id": link})
+            except:
+                continue
+
+            queue_job("crawler.Fetch", {
+                "url": link,
+                "from": params["url"]
+            }, queue="crawl")
+            queued_count += 1
+
+        stored_data = {
+            "_id": response.url,
+            "queued_urls": queued_count,
+            "html_length": len(response.content),
+            "fetched_date": datetime.datetime.now()
+        }
+
+        collection.update(
+            {"_id": response.url},
+            stored_data,
+            upsert=True
+        )
+
+        return True
+
+
+class Report(Task):
+
+    def run(self, params):
+
+        collection = connections.mongodb_jobs.simple_crawler_urls
+
+        print
+        print "Crawl stats"
+        print "==========="
+        print "URLs queued: %s" % collection.find().count()
+        print "URLs successfully crawled: %s" % collection.find({"fetched_date": {"$exists": True}}).count()
+        print "URLs redirected: %s" % collection.find({"redirected_to": {"$exists": True}}).count()
+        print "Bytes fetched: %s" % (collection.aggregate(
+            {"$group": {"_id": None, "sum": {"$sum": "$html_length"}}}
+        )["result"] or [{}])[0].get("sum", 0)
+        print
+
+
+class Reset(Task):
+
+    def run(self, params):
+
+        collection = connections.mongodb_jobs.simple_crawler_urls
+
+        collection.remove({})
+
+        Queue("crawl").empty()
