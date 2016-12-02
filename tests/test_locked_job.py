dd0d02759f0f04905845e88e0774282f54a0706c
Geoffrey Gueret <geoffrey@pricingassistant.com>
2016-12-02 16:35:34 +0100

    worker.stop will stop mongodb too

--- tests/test_locked_job.py
+++ tests/test_locked_job.py
@@ -12,7 +12,6 @@ def test_locked_job(worker):
       worker.send_task("tests.tasks.lock.Locked", {"queue": "default"}, block=False))
 
   worker.wait_for_tasks_results(job_ids, accept_statuses=["success", "failed", "expired"])
-  worker.stop()
 
   statuses = [Job(job_id).fetch().data["status"] for job_id in job_ids]
   assert "success" in statuses
