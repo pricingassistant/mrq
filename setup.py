e29afb7bdb6cf195b3cf11b462f9e9b012c7e0f3
Adrien Chauve <adrien.chauve@serenytics.com>
2016-12-09 19:13:37 +0100

    Add new python 3 supported versions in setup.py

--- setup.py
+++ setup.py
@@ -60,6 +60,8 @@ setup(
     classifiers=[
         "Programming Language :: Python",
         "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
         #'Development Status :: 1 - Planning',
         #'Development Status :: 2 - Pre-Alpha',
         #'Development Status :: 3 - Alpha',
