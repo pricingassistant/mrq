c0cb03d92d9bb75756b828e77dfc4a82130a395f
ismael <ismael@pricingassistant.com>
2018-06-29 17:59:15 +0200

    missing imports

--- mrq/context.py
+++ mrq/context.py
@@ -2,7 +2,7 @@ from future import standard_library
 standard_library.install_aliases()
 from future.builtins import next, map
 from past.builtins import basestring
-# from .logger import Logger
+from .logger import Logger
 import logging
 import gevent
 import gevent.pool
