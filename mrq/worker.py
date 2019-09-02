12bcd408b0d73f099d47c13339a60de65fbcbc93
kraptor <7249728+kraptor@users.noreply.github.com>
2019-09-02 16:24:00 +0200

    Update to support renaming of LuaLock to Lock in redis-py (#214)
    * redis-py renamed LuaLock to lock
    
    * redis-py renamed LuaLock to Lock

--- mrq/worker.py
+++ mrq/worker.py
@@ -15,7 +15,12 @@ import sys
 import json as json_stdlib
 import ujson as json
 from bson import ObjectId
-from redis.lock import LuaLock
+
+try:
+    from redis.lock import LuaLock
+except ImportError:
+    from redis.lock import Lock
+    
 from collections import defaultdict
 from mrq.utils import load_class_by_path
 
